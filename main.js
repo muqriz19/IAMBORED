a7b855dc3bff1f0ac173f3928c0da791c6eb4747
muqriz <muqriz19othman@gmail.com>
2018-12-22 23:01:03 +0800

    basic request

--- main.js
+++ main.js
@@ -13,6 +13,23 @@ function init(){
 
 
 document.querySelector('#search-button').addEventListener('click', function(){
-    console.log('Test');
+    let searchQuery = document.getElementById('search-box').value;
+    console.log(searchQuery);
+
+    searchActivity(searchQuery);
+
+
 });
+
+function searchActivity(query) {
+    let headers = new Headers({
+
+    });
+
+    fetch(`https://www.boredapi.com/api/activity/${query}`).then(res => res.json()).then(function(data){
+        console.log(data);
+    }).catch(function(err) {
+
+    });
+}
 
